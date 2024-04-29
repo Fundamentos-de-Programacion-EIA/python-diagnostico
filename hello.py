3856ae63604df3c7ba8fdce31946b12a65dc6f28
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2024-04-29 10:48:02 -0500

    feat(clases.ipynb): add examples of dictionary and class usage for educational purposes feat(hello.py): add basic Python script demonstrating arithmetic operations and data structures
    feat(vectores.ipynb): add Jupyter notebook with examples of working with lists and operations on them

---
+++ hello.py
@@ -0,0 +1,58 @@
+
+# imprimir un hola mundo
+print("hola mundo")
+
+variable = 10
+
+variable2 = 5
+
+# operación aritmética entre dos números
+resultado = variable + variable2
+
+print("el resultado es: ", resultado)
+
+variable_texto = "Mi nombre es Daniel"
+variable_texto2 = 'Mi apellido es Saldarriaga'
+
+# operación aritmética entre dos textos
+resultado2 = variable_texto + variable_texto2
+
+print("resultado 2: ", resultado2)
+
+# ejemplo de error: operación artimética entre dos tipos distintos
+# resultado3 = variable2 + variable_texto
+
+# print("resultado 3: ", resultado3)
+
+
+# definición de una lista: vector de una dimensión
+lista_1 = [123, 634, 915, 300]
+
+print(lista_1[1])
+
+# cambio del valor del elemento de la lista según su posición
+lista_1[0] = 145
+
+print(lista_1)
+
+# iteración de los elementos de una lista
+for elemento in lista_1:
+    print(elemento)
+
+# for i in range(4):
+#     print(lista_1[i])
+
+variable_entrada_ejemplo = 0
+# ejemplo de ciclo while.
+while variable_entrada_ejemplo == 0:
+    print("variable es igual a 0")
+    variable_entrada_ejemplo = input("ingrese un valor")
+
+
+
+
+
+
+
+
+
